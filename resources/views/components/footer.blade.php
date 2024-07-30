057e47a2e7bf9975da9c3ad5501341d0b30d3064
Jacopo Fornaro <jacopofornaro@gmail.com>
2024-07-30 13:28:00 +0200

    iniziata welcome navbar footer e layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="py-3 my-4 footer mb-0">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Home</a></li>
+        <li class="nav-item"><a class="nav-link px-2 text-body-secondary">Chi sono</a></li>
+        <li class="nav-item"><a class="nav-link px-2 text-body-secondary">Contatti</a>
+        </li>
+    </ul>
+    <p class="text-center text-body-secondary">© 2024 Jacopo Fornaro</p>
+
+</footer>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar1 navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <button class="nav-link dropdown-toggle active" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Informazioni
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item">Biografia</a></li>
+
+                        <li><a class="dropdown-item">Contatti</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
